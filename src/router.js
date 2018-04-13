1239410a0c578baf03c40fa7a88668ae030c1f13
a774285192 <1160001512@qq.com>
2018-04-13 16:46:25 +0800

    Signed-off-by: a774285192 <1160001512@qq.com>

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 
@@ -7,8 +8,11 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
+   
+    
+// ,
+     {
+      path: '/:id',
       name: 'home',
       component: Home
     },
